40550a946410302835eabbd2abcc703c8ad5ba28
Pezhman <github@pezhmankasraee.com>
2025-04-30 09:57:46 +0200

    feat: add student service
    	- able to add one student
    	- able to get all students

---
+++ src/main/java/com/pezhmankasraee/jakartarestbasic/resources/StudentResourceV0.java
@@ -0,0 +1,47 @@
+package com.pezhmankasraee.jakartarestbasic.resources;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pezhmankasraee.jakartarestbasic.models.Student;
+import com.pezhmankasraee.jakartarestbasic.service.StudentService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("v0/students")
+public class StudentResourceV0 {
+
+
+    @Inject
+    private StudentService studentService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Student> getStudents() {
+
+        return this.studentService.getAllStudents();
+    }
+
+    @POST()
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addStudent(String studentJsonString) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Student student = objectMapper.readValue(studentJsonString, Student.class);
+            studentService.add(student);
+            return Response.accepted().build();
+        } catch (JsonProcessingException e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+    }
+
+}
+
+
---
+++ src/main/java/com/pezhmankasraee/jakartarestbasic/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.pezhmankasraee.jakartarestbasic.service;
+
+import com.pezhmankasraee.jakartarestbasic.models.Student;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class StudentService {
+
+    private final List<Student> studentList = new ArrayList<>();
+
+
+    public List<Student> getAllStudents() {
+        return this.studentList;
+    }
+
+    public void add(Student student) {
+        this.studentList.add(student);
+    }
+}
