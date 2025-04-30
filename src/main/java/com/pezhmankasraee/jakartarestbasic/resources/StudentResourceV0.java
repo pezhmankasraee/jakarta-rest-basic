184b8dfddf9d490df1268e91f2453aca9caad264
Pezhman <github@pezhmankasraee.com>
2025-04-30 14:30:03 +0200

    feat: add list of Students

--- src/main/java/com/pezhmankasraee/jakartarestbasic/resources/StudentResourceV0.java
+++ src/main/java/com/pezhmankasraee/jakartarestbasic/resources/StudentResourceV0.java
@@ -1,6 +1,7 @@
 package com.pezhmankasraee.jakartarestbasic.resources;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pezhmankasraee.jakartarestbasic.models.Student;
 import com.pezhmankasraee.jakartarestbasic.service.StudentService;
@@ -9,12 +10,10 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Path("v0/students")
 public class StudentResourceV0 {
-
 
     @Inject
     private StudentService studentService;
@@ -26,6 +25,7 @@ public class StudentResourceV0 {
         return this.studentService.getAllStudents();
     }
 
+    @Path("/single")
     @POST()
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -35,6 +35,21 @@ public class StudentResourceV0 {
         try {
             Student student = objectMapper.readValue(studentJsonString, Student.class);
             studentService.add(student);
+            return Response.accepted().build();
+        } catch (JsonProcessingException e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addList(String studentJsonListOfString) {
+
+        var objectMapper = new ObjectMapper();
+        try {
+            List<Student> studentList = objectMapper.readValue(studentJsonListOfString, new TypeReference<List<Student>>() {});
+            studentService.addAll(studentList);
             return Response.accepted().build();
         } catch (JsonProcessingException e) {
             return Response.status(Response.Status.BAD_REQUEST).build();
